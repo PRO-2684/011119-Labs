45cdad2d60854ef24d26da39127416d682a9e5c2
PRO-2684 <54608551+PRO-2684@users.noreply.github.com>
2024-06-03 13:55:13 +0800

    Capital fix

--- lab2/part_1/DecisionTree.py
+++ lab2/part_1/DecisionTree.py
@@ -5,26 +5,26 @@ import numpy as np
 import pandas as pd
 
 
-# metrics
+# Metrics
 def accuracy(y_true, y_pred):
     return sum(y_true == y_pred) / len(y_true)
 
 
-# model
+# Model
 class DecisionTreeClassifier:
     def __init__(self) -> None:
         self.tree = None
 
-    def fit(self, X, y):
-        # X: [n_samples_train, n_features],
+    def fit(self, x, y):
+        # x: [n_samples_train, n_features],
         # y: [n_samples_train, ],
         # TODO: implement decision tree algorithm to train the model
         pass
 
-    def predict(self, X):
-        # X: [n_samples_test, n_features],
+    def predict(self, x):
+        # x: [n_samples_test, n_features],
         # return: y: [n_samples_test, ]
-        y = np.zeros(X.shape[0])
+        y = np.zeros(x.shape[0])
         # TODO:
         return y
 
@@ -53,26 +53,26 @@ def load_data(datapath: str = "./data/ObesityDataSet_raw_and_data_sinthetic.csv"
         "MTRANS": 5,
     }
 
-    X, y = df.iloc[:, :-1], df.iloc[:, -1]
-    # encode discrete str to number, eg. male&female to 0&1
+    x, y = df.iloc[:, :-1], df.iloc[:, -1]
+    # Encode discrete str to number, e.g. male&female to 0&1
     labelencoder = LabelEncoder()
     for col in discrete_features:
-        X[col] = labelencoder.fit(X[col]).transform(X[col])
+        x[col] = labelencoder.fit(x[col]).transform(x[col])
     y = labelencoder.fit(y).fit_transform(y)
 
-    X_train, X_test, y_train, y_test = train_test_split(
-        X, y, test_size=0.2, random_state=42
+    x_train, x_test, y_train, y_test = train_test_split(
+        x, y, test_size=0.2, random_state=42
     )
 
-    return X_train, X_test, y_train, y_test
+    return x_train, x_test, y_train, y_test
 
 
 if __name__ == "__main__":
-    X_train, X_test, y_train, y_test = load_data(
+    x_train, x_test, y_train, y_test = load_data(
         "./data/ObesityDataSet_raw_and_data_sinthetic.csv"
     )
     clf = DecisionTreeClassifier()
-    clf.fit(X_train, y_train)
+    clf.fit(x_train, y_train)
 
-    y_pred = clf.predict(X_test)
+    y_pred = clf.predict(x_test)
     print(accuracy(y_test, y_pred))
